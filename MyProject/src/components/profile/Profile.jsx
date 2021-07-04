4edd6e30f0616f14209cb97fab0a6bfc28d0e01d
6akcbaHu <>
2021-07-04 12:22:45 +0300

    update Header and NavBAr

--- MyProject/src/components/profile/Profile.jsx
+++ MyProject/src/components/profile/Profile.jsx
@@ -5,10 +5,18 @@ import MyPost from '../myPost/MyPost';
 const Profile = () => {
     return (
         <div className={s.content}>
-            <div>
-                <img src="https://vjoy.cc/wp-content/uploads/2019/07/1-1.jpg" alt=""/>
+            <div className={s.profile}>
+                <div className={s.profile__logo}>
+                    <img src="https://www.meme-arsenal.com/memes/4522023f3e25467b5328d24596676806.jpg" alt=""/>
+                </div>
+                <div className="profile__description">
+                    <div className="pd__name">Dima D</div>
+                    <div className="pd__birthday">15.12.89</div>
+                    <div className="pd__city">Moscow</div>
+                    <div className="pd__education">BGTU'16</div>
+                    <div className="pd__site">www.ssssss.com</div>
+                </div>
             </div>
-            <div>AvaDicriptions</div>
             <MyPost/>
         </div>
     );
