c5204b3f40ff7142e5d5512e661ef218a4d84402
6akcbaHu <>
2021-07-20 19:05:39 +0300

    React Component

--- MyProject/src/componets/Users/Users.jsx
+++ MyProject/src/componets/Users/Users.jsx
@@ -1,93 +1,91 @@
 import React from "react";
 import s from "./Users.module.css"
-
-const Users = (props) => {
-
-    if (props.users.length === 0) {
-        props.setUsers([
-            {
-                id: 1,
-                followed: false,
-                name: 'Nikolay',
-                img: 'https://img.megaobzor.com/old/alia/895.jpg',
-                location: {city: 'Perm', conutry: 'Russia'}
-            },
-            {
-                id: 2,
-                name: 'Jacob',
-                followed: true,
-                img: 'https://i1.sndcdn.com/artworks-000111336685-1cd0h6-t500x500.jpg',
-                location: {city: 'Philadelphia,state of Pennsylvania', conutry: 'USA'}
-            },
-            {
-                id: 3,
-                followed: false,
-                name: 'Nikolay',
-                img: 'https://img.megaobzor.com/old/alia/895.jpg',
-                location: {city: 'Perm', conutry: 'Russia'}
-            },
-            {
-                id: 4,
-                name: 'Jacob',
-                followed: true,
-                img: 'https://i1.sndcdn.com/artworks-000111336685-1cd0h6-t500x500.jpg',
-                location: {city: 'Philadelphia,state of Pennsylvania', conutry: 'USA'}
-            },
-            {
-                id: 5,
-                followed: false,
-                name: 'Nikolay',
-                img: 'https://img.megaobzor.com/old/alia/895.jpg',
-                location: {city: 'Perm', conutry: 'Russia'}
-            },
-
-            {
-                id: 6,
-                name: 'Jacob',
-                followed: true,
-                img: 'https://i1.sndcdn.com/artworks-000111336685-1cd0h6-t500x500.jpg',
-                location: {city: 'Philadelphia,state of Pennsylvania', conutry: 'USA'}
-            },
-            {
-                id: 7,
-                followed: false,
-                name: 'Nikolay',
-                img: 'https://img.megaobzor.com/old/alia/895.jpg',
-                location: {city: 'Perm', conutry: 'Russia'}
-            },
+import * as axios from "axios";
+import AvaUsers from "../../images/users.jpg";
 
 
+// const Users = (props) => {
+//
+//     if (props.users.length === 0) {
+//         axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response=>{
+//                    props.setUsers(response.data.items)
+//     })
+//     }
+//
+//     return (
+//
+//
+//         <div className={s.users}>
+//             <div className={s.users__input}>
+//                 <input type="text"/>
+//             </div>
+//
+//             {props.users.map(u =>
+//
+//                 <div key={u.id} className={s.users__item}>
+//                     <div className={s.users__item__foto}>
+//                         <img src={u.photos.small!=null?u.photos.small:AvaUsers}/>
+//                     </div>
+//                     <div className={s.users__item__name}>
+//                         <p>{u.name}</p>
+//                         <p>{'u.location.city'}</p>
+//                         <p><p>{'u.location.conutry'}</p></p>
+//                     </div>
+//                     <div className={s.users__item__button}>
+//                         {u.followed
+//                         ? <button onClick={()=>
+//                         props.unfollow(u.id)}>unfollow</button>
+//                         :<button onClick={()=>
+//                         props.follow(u.id)}>follow</button>}
+//                     </div>
+//                 </div>
+//             )}
+//         </div>
+//     )
+// }
+class Users extends React.Component {
+    constructor(props) {
+        super(props);
 
-        ])
+        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+            props.setUsers(response.data.items)
+        })
     }
-    console.log(Users)
-    return (
+
+    render() {
 
 
-        <div className={s.users}>
-            <div className={s.users__input}>
-                <input type="text"/>
-            </div>
-            {props.users.map(u =>
-                <div key={u.id} className={s.users__item}>
-                    <div className={s.users__item__foto}>
-                        <img src={u.img}/>
-                    </div>
-                    <div className={s.users__item__name}>
-                        <p>{u.name}</p>
-                        <p>{u.location.city}</p>
-                        <p><p>{u.location.conutry}</p></p>
-                    </div>
-                    <div className={s.users__item__button}>
-                        {u.followed
-                        ? <button onClick={()=>
-                        props.unfollow(u.id)}>unfollow</button>
-                        :<button onClick={()=>
-                        props.follow(u.id)}>follow</button>}
-                    </div>
+        return (
+
+
+            <div className={s.users}>
+                <div className={s.users__input}>
+                    <input type="text"/>
                 </div>
-            )}
-        </div>
-    )
+
+                {this.props.users.map(u =>
+
+                    <div key={u.id} className={s.users__item}>
+                        <div className={s.users__item__foto}>
+                            <img src={u.photos.small != null ? u.photos.small : AvaUsers}/>
+                        </div>
+                        <div className={s.users__item__name}>
+                            <p>{u.name}</p>
+                            <p>{'u.location.city'}</p>
+                            <p><p>{'u.location.conutry'}</p></p>
+                        </div>
+                        <div className={s.users__item__button}>
+                            {u.followed
+                                ? <button onClick={() =>
+                                    this.props.unfollow(u.id)}>unfollow</button>
+                                : <button onClick={() =>
+                                    this.props.follow(u.id)}>follow</button>}
+                        </div>
+                    </div>
+                )}
+            </div>
+        )
+    }
 }
+
 export default Users;
